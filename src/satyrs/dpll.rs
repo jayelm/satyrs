768849e12aedd48a241e3f86798024ca542fc5a8
Kevin Gleason <kaagleason@gmail.com>
2016-05-07 14:37:06 -0400

    Not sure

--- src/satyrs/dpll.rs
+++ src/satyrs/dpll.rs
@@ -1,5 +1,7 @@
 use satyrs::cnf::CNF;
 
+
+
 /**
  * DPLL Algorithm implementation
  * Start by cloning the cnf so we can modify the HashMap
@@ -8,6 +10,12 @@ use satyrs::cnf::CNF;
 #[allow(non_snake_case)]
 pub fn DPLL( cnf : &CNF ) -> bool {
     let cnf_clone = cnf.clone();
+    // If consistent set of literals, return True
+    // If contains an empty clause return False
+    // For every unit-clause, unit-propogate
+    // For ever pure literal, pure literal assign
+    // Choose literal L for split
+    // Return DPLL with L and -L
     println!("{:?}",cnf_clone);
     true
 }
